2d23519b21d9928e0bc510f4e5900342058dbe6b
elmogyy <62273194+elmogyy@users.noreply.github.com>
2025-01-19 19:54:58 +0200

    Add files via upload

---
+++ FOS_Assignment_Template/kern/tests.c
@@ -0,0 +1,24 @@
+#include <kern/tests.h>
+
+//define the white-space symbols
+#define WHITESPACE "\t\r\n "
+
+void TestAssignment1()
+{
+	cprintf("\n========================\n");
+	cprintf("Automatic Testing of Q1:\n");
+	cprintf("========================\n");
+	TestAss1Q1();
+	cprintf("\n========================\n");
+	cprintf("Automatic Testing of Q2:\n");
+	cprintf("========================\n");
+	TestAss1Q2();
+}
+int TestAss1Q1()
+{
+	return 0;
+}
+int TestAss1Q2()
+{
+	return 0;
+}
